c8245c8ca645ea39dc4fae772856c89a9ea8ad5a
vijay <vijay.m@tectoro.com>
2018-12-01 22:47:43 +0530

    new files added.

---
+++ src/app/pages/user/profile/profile.service.ts
@@ -0,0 +1,17 @@
+import {Observable }from 'rxjs'; 
+import {Injectable }from '@angular/core'; 
+import {HttpClient }from '@angular/common/http'; 
+import {environment as process }from '../../../../environments/environment'; 
+
+
+@Injectable()
+export class ProfileService {
+
+    constructor(private http:HttpClient) {}
+
+    updateUser(userDetails):Observable < any >  {
+      return  this.http.put(process.url + "/userupdate", userDetails)
+    
+    }
+
+}
